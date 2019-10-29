01ebdd268db146a84c4f00263c49bc76c931feda
PipZip <37911683+PipZip@users.noreply.github.com>
2019-10-29 16:55:08 +0600

    temp

--- src/js/partials/main.js
+++ src/js/partials/main.js
@@ -1,6 +1,14 @@
 $(document).ready(function() {
 
+    $('.marquee').marquee({
+        duration: 15000,
+        gap: 0,
+        delayBeforeStart: 0,
+        direction: 'left',
+        duplicated: true,
+        startVisible:true
 
+    });
 
 });
 
